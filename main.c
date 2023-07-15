8aa21af4101a1f0651e8a09dbfd2f4648240590d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-14 23:21:42 -0300

    attt

--- main.c
+++ main.c
@@ -25,18 +25,18 @@ int main(){
     load_exemple_tree();
 
     //exemple_folder->insecure_hardware_write_tree(exemple_folder);
-    exemple_folder->represent(exemple_folder);
-    char *destination = "test";
+    char *destination = "test/";
 
     //modifying the tree
     for(int i = 0 ; i <exemple_folder->size; i++){
         DtwTreePart *current_part = exemple_folder->tree_parts[i];
         DtwPath *current_path = current_part->path;
         current_path->add_start_dir(current_path,destination);
-        current_part->hardware_write(current_part,DTW_SET_AS_ACTION);
+        printf("--------------------------\n");
+        current_path->represent(current_path);
     }
-
-    exemple_folder->hardware_commit_tree(exemple_folder);
+    //exemple_folder->represent(exemple_folder);
+    //exemple_folder->hardware_commit_tree(exemple_folder);
     exemple_folder->free(exemple_folder);
 
 }
