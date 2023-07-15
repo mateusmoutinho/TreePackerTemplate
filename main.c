0bc0e4064a25c2bce88f9a0beb0d16a7fb45a8a7
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-15 03:40:52 -0300

    att

--- main.c
+++ main.c
@@ -23,19 +23,32 @@ void load_exemple_tree(){
 int main(){
 
     load_exemple_tree();
+    CliInterface  interface = newCliInterface();
 
-    //exemple_folder->insecure_hardware_write_tree(exemple_folder);
-    char *destination = "test/";
+
+    char *destination =  interface.ask_string(&interface,"inform the destionation: ",CLI_TRIM);
 
     //modifying the tree
     for(int i = 0 ; i <exemple_folder->size; i++){
         DtwTreePart *current_part = exemple_folder->tree_parts[i];
         DtwPath *current_path = current_part->path;
         current_path->add_start_dir(current_path,destination);
-    
     }
-    exemple_folder->represent(exemple_folder);
-    exemple_folder->hardware_commit_tree(exemple_folder);
+    DtwTreeTransactionReport *report = exemple_folder->report(exemple_folder);
+
+    interface.print(&interface,"the foolowing transaction will be executed\n");
+    report->represent(report);
+
+    bool execute = interface.ask_option(&interface,"continue ? (yes,no)","yes | no");
+    if(execute){
+        exemple_folder->hardware_commit_tree(exemple_folder);
+        interface.print(&interface,"the following folder were copied");
+    }
+    else{
+        interface.warning(&interface,"transacton aborted");
+    }
+
+    report->free(report);
     exemple_folder->free(exemple_folder);
 
 }
