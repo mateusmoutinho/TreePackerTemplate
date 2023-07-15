9b4dfbdba880051635d25f3933866cfe642715cc
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-14 23:22:14 -0300

    att

--- main.c
+++ main.c
@@ -32,11 +32,10 @@ int main(){
         DtwTreePart *current_part = exemple_folder->tree_parts[i];
         DtwPath *current_path = current_part->path;
         current_path->add_start_dir(current_path,destination);
-        printf("--------------------------\n");
-        current_path->represent(current_path);
+    
     }
-    //exemple_folder->represent(exemple_folder);
-    //exemple_folder->hardware_commit_tree(exemple_folder);
+    exemple_folder->represent(exemple_folder);
+    exemple_folder->hardware_commit_tree(exemple_folder);
     exemple_folder->free(exemple_folder);
 
 }
